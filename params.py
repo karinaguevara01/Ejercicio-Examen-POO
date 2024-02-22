cc3861700507e946f4af95875a80bdd3f24972ce
karinaguevara01 <159199847+karinaguevara01@users.noreply.github.com>
2024-02-22 13:09:55 -0600

    Create params.py

---
+++ params.py
@@ -0,0 +1,7 @@
+menu=["***********MENU****************",
+      "1. Agregar",
+      "2. Mostrar contratos",
+      "3. Mostrar Contrato",
+      "4. Buscar estado",
+      "5. Modificar Contrato",
+      "6. Salir"]
